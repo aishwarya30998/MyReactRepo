10c274044bc2a630c8e0ea0ebe39279f2a201a2c
aishwarya30998 <aishwarya.ap998@gmail.com>
2023-07-14 17:57:54 -0400

    made required changes to understand useEffect

--- my-login/src/App.js
+++ my-login/src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+///useeffect is used to prevent infinite loop
+//normally without this useeffect components rerun when state changes, but 
+//here it reruns on state change and also change in specified value dependency toooo!!!
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation == '1'){
+    setIsLoggedIn(true);
+  }}, [] );
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
